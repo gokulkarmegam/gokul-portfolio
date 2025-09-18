05af8918b42619285467bc65d47d9a3a3907ae31
gokulkarmegam <gokulakannankarmegam.com>
2025-09-18 23:59:18 +0530

    Fix vite.config.js base path

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      base: '/gokul-portfolio/',  //
     },
   },
+  base: '/gokul-portfolio/', // 👈 must be here, not inside resolve
 })
